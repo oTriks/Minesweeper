7fa5b5a884030f6a28a6a0b13482aab021418578
Ina Burström <inaburstrom@hotmail.com>
2023-10-24 13:32:26 +0200

    isMine method done

--- src/Mines.java
+++ src/Mines.java
@@ -5,16 +5,28 @@ public class Mines {
     private String symbol = "X";
     private int quantity = 10;
 
-    public Mines () {
+    public Mines() {
         setupField();
     }
 
-    public void setupField(){
+    public void setupField() {
         while (quantity != 0) {
-            int row = random.nextInt(10);
-            int col = random.nextInt(10);
-            System.out.println(row + " + " + col);
-            quantity--;
+            int randomRow = random.nextInt(10);
+            int randomCol = random.nextInt(10);
+            //System.out.println(randomRow + " + " + randomCol);
+            if ( !isMine();){
+                quantity--;
+            }
+
+        }
+    }
+
+    //Put into GameBoard class and change names when merged
+    public boolean isMine() {
+        if (GameBoard[row][col] == "X") {
+            return true;
+        } else {
+            return false;
         }
     }
 
