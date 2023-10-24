b1f8790e38b24b2763974b789c592cd7c947f603
Ina Burström <inaburstrom@hotmail.com>
2023-10-24 14:16:05 +0200

    placeMine method added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        Mines mineField = new Mines();
+        System.out.println("Hello world!");
     }
 }
--- src/Mines.java
+++ src/Mines.java
@@ -2,6 +2,8 @@ import java.util.Random;
 
 public class Mines {
     Random random = new Random();
+    int randomRow;
+    int randomCol;
     private String symbol = "X";
     private int quantity = 10;
 
@@ -11,23 +13,22 @@ public class Mines {
 
     public void setupField() {
         while (quantity != 0) {
-            int randomRow = random.nextInt(10);
-            int randomCol = random.nextInt(10);
+            randomRow = random.nextInt(10);
+            randomCol = random.nextInt(10);
             //System.out.println(randomRow + " + " + randomCol);
-            if ( !isMine();){
+            if (!isMine()){
+                placeMine();
                 quantity--;
             }
-
         }
     }
 
-    //Put into GameBoard class and change names when merged
     public boolean isMine() {
-        if (GameBoard[row][col] == "X") {
-            return true;
-        } else {
-            return false;
-        }
+        return GameBoard[randomRow][randomCol].equals("X");
+    }
+
+    public void placeMine() {
+        GameBoard[randomRow][randomCol] = "X";
     }
 
 }
