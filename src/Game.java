17bb34e81d989d9b09b27fd6a33e995acc2c9896
DESKTOP-UIEJSCU\oTriks <maar_tiin@hotmail.com>
2023-10-24 13:39:30 +0200

    makeMove, timer

--- src/Game.java
+++ src/Game.java
@@ -1,9 +1,27 @@
 public class Game {
 
-    timer
 
 
-    makeMove
+    // long start = System.currentTimeMillis(); // anropa programmets start
+    // long stop = System.currentTimeMillis(); // anropa programmets slut
+    //   long totalTime = stop - start;
+    //  System.out.println("Time: " + totalTime + " s");
 
 
+
+    public void makeMove(String choice){
+
+        char row = choice.charAt(0);
+        int column = Integer.parseInt(choice.substring(1, 2)) -1;
+        int rowNumber = Character.toUpperCase(row) - 65;
+        this.board[rowNumber][column];
+        // kontroll valdes en mina
+        // valid choice?
+        // anrop siffra avstånd till mina
+        int number = 0; // anropet
+        this.board[rowNumber][column] = number;
+        if(number == 0){
+            // anropa öppna tomma rutor automatiskt
+        }
+    }
 }
