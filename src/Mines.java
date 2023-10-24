423572984ec5356e9344fd194ca1e11fa8fbf3b4
Ina Burström <inaburstrom@hotmail.com>
2023-10-24 12:23:35 +0200

    Added setupField method and random function.

--- src/Mines.java
+++ src/Mines.java
@@ -1,3 +1,21 @@
+import java.util.Random;
+
 public class Mines {
+    Random random = new Random();
     private String symbol = "X";
+    private int quantity = 10;
+
+    public Mines () {
+        setupField();
+    }
+
+    public void setupField(){
+        while (quantity != 0) {
+            int row = random.nextInt(10);
+            int col = random.nextInt(10);
+            System.out.println(row + " + " + col);
+            quantity--;
+        }
+    }
+
 }
