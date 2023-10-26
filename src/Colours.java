8ecbe278ef3b57e55b25c7d2589370caf89227a9
Oskar Lövstrand <Oskar.lovstrand@hotmail.com>
2023-10-26 09:38:57 +0200

    Added all the colours for numbers shown when adjacent to a mine, up to 8 numbers. Also added colour for the gameboard's row and column characters and the mines.

---
+++ src/Colours.java
@@ -0,0 +1,13 @@
+public class Colours {
+    public static String colour1 = "\033[0;34m"; //code for blue colour.
+    public static String colour2 = "\033[0;32m"; //code for green colour.
+    public static String colour3 = "\033[38;5;88m"; //code for dark red colour.
+    public static String colour4 = "\033[0;35m"; //code for purple colour.
+    public static String colour5 = "\033[38;5;125m"; //code for maroon colour.
+    public static String colour6 = "\033[38;5;121m"; //code for turquoise colour.
+    public static String colour7 = "\033[38;5;16m"; //code for black colour.
+    public static String colour8 = "\033[0;37m"; //code for gray colour.
+    public static String colourMine = "\033[38;5;196m"; //code for the mines' colour. Sharp red.
+    public static String colourBoard = "\033[38;5;226m"; //code for the board´s row and col definers. Bright yellow.
+    public static String colourReset = "\033[0m"; //code to reset previous color to original white.
+}
