416672d70bb49f4fe8875099282208474bce62f5
DESKTOP-UIEJSCU\oTriks <maar_tiin@hotmail.com>
2023-10-31 11:10:30 +0100

    fixed errors, comments

--- src/Colours.java
+++ src/Colours.java
@@ -4,7 +4,7 @@ public class Colours {
     public static String colour0 = "\033[38;5;234m"; //code for almost-invisible gray/black.
     public static String colour1 = "\033[0;34m"; //code for blue colour.
     public static String colour2 = "\033[0;32m"; //code for green colour.
-    public static String colour3 = "\033[38;5;88m"; //code for dark red colour.
+    public static String colour3 = "\033[38;5;126m"; //code for dark red colour.
     public static String colour4 = "\033[0;35m"; //code for purple colour.
     public static String colour5 = "\033[38;5;125m"; //code for maroon colour.
     public static String colour6 = "\033[38;5;121m"; //code for turquoise colour.
@@ -12,7 +12,7 @@ public class Colours {
     public static String colour8 = "\033[0;37m"; //code for gray colour.
     public static String colourQuestionMark = "\033[38;5;95m"; //code for the question marks colour.
     public static String colourMine = "\033[38;5;196m"; //code for the mines' colour. Sharp red.
-    public static String colourPickedMine = "\033[48;5;52m"; //code for the background colour of picked mine.
+
     public static String colourBoard = "\033[38;5;226m"; //code for the board´s row and col definers. Bright yellow.
     public static String colourReset = "\033[0m"; //code to reset previous color to original white.
 
--- src/Game.java
+++ src/Game.java
@@ -16,9 +16,6 @@ public class Game {
     // scans users input, starts game timer and calls methods to validate input and make the move on the board
     public void makeMove() {
         moves++;
-
-        mines.showSolutionBoard();   // TODO ska tas bort, bara facit för felsökning
-
         System.out.println(gameBoard.getBoardLayout());
         System.out.println("Skriv in vilken ruta du vill öppna:");
         String input = sc.nextLine();
@@ -67,13 +64,7 @@ public class Game {
     }
 
 
-    public boolean checkWin() { // checkwin method is to check if the player wins the game or not
-        char[][] board = gameBoard.getBoard(); //calling current board
-        for (int i = 0; i < 9; i++) { // loop going through rows
-            for (int j = 0; j < 9; j++) { // loop going through cols
-                if (board[i][j] == '?' && !mines.isMine(i, j)) { //if the cell is empty or marked with "?" then return false which means no win
-
-
+    // checks if the player wins the game
     public boolean checkWin() {
         char[][] board = gameBoard.getBoard();
         for (int i = 0; i < 9; i++) {
@@ -84,9 +75,10 @@ public class Game {
                 }
             }
         }
-        return true; // if the loop has gone through the entire board without returning false then return true which means its a win
+        return true;
     }
 
+    // Exception handling for cell input
     public int[] isCellValidChoice(String input) {
         int row;
         int col;
@@ -117,45 +109,35 @@ public class Game {
         return result;
     }
 
-    public void checkGameStatus(int row, int col) { // this method is to check if the chosen cell is a mine or if the player win,
-        if (mines.isMine(row, col)) { //if the chosen cell is a mine
-            mines.showSolutionBoard(); //showing the solution board with mine
-            stop = System.currentTimeMillis(); // stop the timerb
-            System.out.println("Game over! Du har träffat en mina! ");
 
-            askToPlayAgain(); // ask if play again
-        } else if (checkWin()) { // if the player won the game
+
+    // this method is to check if the chosen cell is a mine or if the player win
+    public void checkGameStatus(int row, int col) {
+        if (mines.isMine(row, col)) {
             mines.showSolutionBoard();
             stop = System.currentTimeMillis();
-            System.out.println("Grattis! Du har vunnit spelet! ");
-
-            System.out.println("Tid: " + (stop - start) + " s"); // a message to show the total time taken to finish the game
-
+            System.out.println("Game over! Du har träffat en mina! ");
             System.out.println("Tid: " + (stop - start) / 1000 + " s");
             askToPlayAgain();
-
         } else if (checkWin()) {
             mines.showSolutionBoard();
             stop = System.currentTimeMillis();
             System.out.println("Grattis! Du har vunnit spelet! ");
             System.out.println("Tid: " + (stop - start) / 1000 + " s");
-
             askToPlayAgain();
 
         } else {
-            makeMove(); // if the game still going, continue with the next move
+            makeMove();
         }
     }
 
 
     public void askToPlayAgain() {
-      
-   
-          // If the user wants to play again, reset the game by creating new instances of GameBoard and Mines
-          System.out.print("Vill du spela igen? (ja/nej): ");
+        // If the user wants to play again, reset the game by creating new instances of GameBoard and Mines
+        System.out.print("Vill du spela igen? (ja/nej): ");
         String playAgain = sc.nextLine();
 
-        if (playAgain.trim().equalsIgnoreCase("ja")) {  
+        if (playAgain.trim().equalsIgnoreCase("ja")) {
             moves = 0;
             gameBoard = new GameBoard();
             mines = new Mines();
@@ -167,9 +149,6 @@ public class Game {
         } else {
             System.out.println("Ogiltig inmatning. Vänligen svara med ja/nej.");
             askToPlayAgain();
-
         }
     }
 }
-
-
